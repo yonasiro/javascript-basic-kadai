6d4bc9a9375692d5b679de3bb829e48851a58253
yamada760 <153713243+yonasiro@users.noreply.github.com>
2024-01-15 12:01:35 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,12 @@
+const holidays =  ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+let index=0;
+while(index<holidays.length){
+console.log(holidays[index]);
+index++;
+}
+
+console.log(holidays);
+for (let i = 1; i <=0; i++) {
+    console.log(i);
+  }
+
